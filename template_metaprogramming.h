836a216db300e024753b3340975eaea2af61796d
mabaro <markisbcn@gmail.com>
2020-09-29 17:23:14 +0200

    Create template_metaprogramming.h
    initial commit with some useful trick for template parameter deduction

---
+++ template_metaprogramming.h
@@ -0,0 +1,19 @@
+//https://artificial-mind.net/blog/2020/09/26/dont-deduce
+
+template <class T>
+struct dont_deduce_t 
+{
+    using type = T;
+};
+
+template <class T>
+using dont_deduce = typename dont_deduce_t<T>::type;
+
+// USE CASES
+
+template<typename T>
+//T myFunc(const std::vector<T>& myVec, T value1, T value2)
+// with dont_deduce the T is deduced only from the first parameter, forcing the other T references to that type
+T myFunc(const std::vector<T>& myVec,  dont_deduce<T> value1, dont_deduce<T> value2)
+{
+}
